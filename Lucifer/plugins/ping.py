1df3be582f5c3803ca05e0c6679a7ea8b81d50af
N1xDOMINATOR <113828214+DOMINATOR-XD@users.noreply.github.com>
2022-09-21 21:56:03 +0530

    Update ping.py

--- Lucifer/plugins/ping.py
+++ Lucifer/plugins/ping.py
@@ -51,8 +51,7 @@ async def _(event):
     x = await eor(event, "⛝ ＰＯＮＧ! ⛝")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    if PING_IMG:
-        "https://telegra.ph/file/42a423c45e4146cf8a94c.mp4"
+    if PING_IMG:"https://telegra.ph/file/42a423c45e4146cf8a94c.mp4"
     uptime = get_readable_time((time.time() - StartTime))
     await x.edit(
         f"{PING_IMG}\n\n✘ **🥀ριиg🥀** : `{ms}`\n✘ **💖υρтιмє💖** : `{uptime}`\n✘ **𝐌𝐘 𝐏𝐄𝐑𝐎 𝐌𝐀𝐒𝐓𝐄𝐑** : [{DEFAULTUSER}](tg://user?id={OWNER_ID})\n\n© [𝙻ucifer 𝚇 𝚄𝚂𝙴𝚁𝙱𝙾𝚃](https://t.me/dominator_bot_official)"
