c13f8e56e00ac7dc736c6dec97a520f69c145cf9
N1xDOMINATOR <113828214+DOMINATOR-XD@users.noreply.github.com>
2022-09-21 23:15:02 +0530

    Update ping.py

--- Lucifer/plugins/ping.py
+++ Lucifer/plugins/ping.py
@@ -49,14 +49,12 @@ async def _(event):
         return
     start = datetime.now()
     x = await eor(event, "⛝ ＰＯＮＧ! ⛝")
-
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    uptime = get_readable_time((time.time()  StartTime))
-   caption = f"✘ 🥀ριиg🥀 : {ms}\n✘ 💖υρтιмє💖 : {uptime}\n✘ 𝐌𝐘 𝐏𝐄𝐑𝐎 𝐌𝐀𝐒𝐓𝐄𝐑 : [{DEFAULTUSER}](tg://user?id={OWNER_ID})\n\n© [𝙻ucifer 𝚇 𝚄𝚂𝙴𝚁𝙱𝙾𝚃](https://t.me/dominator_bot_official)"
-   ping_file = "https://telegra.ph/file/42a423c45e4146cf8a94c.mp4"
-    await event.send_file(chat , file=ping_file , caption=caption
-        )
+    uptime = get_readable_time((time.time() - StartTime))
+    await x.edit(
+        f"https://telegra.ph/file/42a423c45e4146cf8a94c.mp4\n\n✘ 🥀ριиg🥀 : {ms}\n✘ 💖υρтιмє💖 : {uptime}\n✘ 𝐌𝐘 𝐏𝐄𝐑𝐎 𝐌𝐀𝐒𝐓𝐄𝐑 : [{DEFAULTUSER}](tg://user?id={OWNER_ID})\n\n© [𝙻ucifer 𝚇 𝚄𝚂𝙴𝚁𝙱𝙾𝚃](https://t.me/dominator_bot_official)"
+    )
 
 
 CMD_HELP.update({"ping": ".ping\nUse - See the ping stats and uptime of userbot."})
