a19bd38313cd4d9155a66964abd3cb2887f4e6f3
N1xDOMINATOR <113828214+DOMINATOR-XD@users.noreply.github.com>
2022-09-25 08:42:10 +0530

    Update ping.py

--- Lucifer/plugins/ping.py
+++ Lucifer/plugins/ping.py
@@ -52,9 +52,8 @@ async def _(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     uptime = get_readable_time((time.time() - StartTime))
-    await message.reply_photo(
-        photo=f"https://telegra.ph/file/42a423c45e4146cf8a94c.mp4",
-        caption=f"✘ **🥀ριиg🥀** : `{ms}`\n✘ **💖υρтιмє💖** : `{uptime}`\n✘ **👑𝐌𝐘 𝐏𝐄𝐑𝐎 𝐌𝐀𝐒𝐓𝐄𝐑👑** : [{DEFAULTUSER}](tg://user?id={OWNER_ID})\n\n© [𝙻ucifer 𝚇 𝚄𝚂𝙴𝚁𝙱𝙾𝚃](https://t.me/dominator_bot_support)"
+    await x.edit(
+        f"[🤗](https://telegra.ph/file/42a423c45e4146cf8a94c.mp4)\n\n✘ **ριиg** : `{ms}`\n✘ **υρтιмє** : `{uptime}`\n✘ **𝐌𝐘 𝐏𝐄𝐑𝐎 𝐌𝐀𝐒𝐓𝐄𝐑** : [{DEFAULTUSER}](tg://user?id={OWNER_ID})\n\n© 𝙻ucifer 𝚇 𝚄𝚂𝙴𝚁𝙱𝙾𝚃"
     )
 
 
